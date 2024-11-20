a1ab8120fe5cf11ddbb525c8412b4a7538f09fec
chibs <chibuikemu13@gmail.com>
2024-11-20 14:35:34 +0000

    Initialize car parking simulation with stacks and user input handling

--- oop-CA2-Collections-STARTER-Nov24-master/src/main/java/org/example/Question2.java
+++ oop-CA2-Collections-STARTER-Nov24-master/src/main/java/org/example/Question2.java
@@ -1,4 +1,6 @@
 package org.example;
+import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *  Name:
@@ -6,9 +8,14 @@ package org.example;
  */
 public class Question2  // Car Parking - Stack
 {
-    public static void runSimulation()
-    {
-    }
+    public static void runSimulation() {
+        Stack<Integer> driveway = new Stack<>();
+        Stack<Integer> street = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Car Parking Simulation");
+        System.out.println("Enter a positive number to add a car, a negative number to remove a car, or 0 to stop:");
+
     public static void main(String[] args) {
         runSimulation();
     }

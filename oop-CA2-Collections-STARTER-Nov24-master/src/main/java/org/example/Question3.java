a18d8e360efe4ee739c8d104e317d8c2ceb7d361
Pooky12332 <pooky12332@gmail.com>
2024-12-09 22:08:06 +0000

    Questions 3 and 6
    - Added Question 6
    - Added Question 3
    - Fixed a syntax error on Question 2 to allow for testing

--- oop-CA2-Collections-STARTER-Nov24-master/src/main/java/org/example/Question3.java
+++ oop-CA2-Collections-STARTER-Nov24-master/src/main/java/org/example/Question3.java
@@ -2,43 +2,75 @@ package org.example;
 
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.File;
+import java.util.Stack;
 
 /**
- *  Name:
- *  Class Group:
+ *  Name: Ewan Clarke
+ *  Class Group: SD2b
  */
-public class Question3  {   //Nested HTML (Stack)
 
-    /*
-filename: name of the file to test.
-*/
-    public static boolean validate(String filename) throws FileNotFoundException
-    {
-        return false;
+public class Question3  {  //Nested HTML (Stack)
+    public static void main(String[] args) {
+        checkTags("index.html");
     }
 
-    /*
-        This function tests the files in the files array to see if
-         they are valid.
-         tags_valid.txt should return true;
-         tags_invalid.txt should output as invalid;
+    public static void checkTags(String filename) {
+        boolean filePassed = true;
 
+        try {
+            File htmlFile = new File(filename);
+            Scanner htmlScanner = new Scanner(htmlFile);
+            while (htmlScanner.hasNextLine()) {
+                String line = htmlScanner.nextLine();
+                filePassed = indexLine(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No file called " + filename + " found.");
+            System.exit(0);
+        }
 
-     */
-    public static void main(String[] args) throws FileNotFoundException {
-        String[] files = {"tags_valid.txt", "tags_invalid.txt"};
-        for(String fName: files) {
-            System.out.print(fName +": ");
-            if (validate(fName)) {
-                System.out.println("This file is valid");
-            } else {
-                System.out.println("This file is invalid");
+        // Basic output depending on linePassed
+        if (filePassed) {
+            System.out.println("All tags in the given file are closed correctly.");
+        } else {
+            System.out.println("A tag in your file doesnt close. Double check your tags");
+        }
+        System.exit(0);
+    }
+
+    public static boolean indexLine(String line) {
+        Stack<String> tagStack = new Stack<>();
+        // Splitting the line via the given space
+        String[] tags = line.split(" ");
+
+        for (String tag : tags) {
+            // Checking to make sure the element is an actual tag
+            if ((tag.startsWith("<")) && (tag.endsWith(">"))) {
+                // Opening tag and closing tag logic
+                if (!tag.startsWith("</")) { // If opening tag:
+                    if (!tag.equals("<br>")) {
+                        tagStack.push(tag);
+                    }
+                } else { // If closing tag:
+                    // Check to see if there is a tag in the stack
+                    String closingTag = tag.substring(2, tag.length() - 1);
+
+                    if (tagStack.isEmpty()) {
+                        return false;
+                    }
+
+                    // Since there are tags in the stack, we take the first one and check to see that they match
+                    String openingTag = tagStack.pop();
+                    String expectedTag = openingTag.substring(1, openingTag.length() - 1);
+
+                    if (!expectedTag.equals(closingTag)) {
+                        return false;
+                    }
+                }
             }
         }
+
+        return tagStack.isEmpty();
     }
-
-
-
 }
-
-
--- oop-CA2-Collections-STARTER-Nov24-master/src/main/java/org/example/Question6.java
+++ oop-CA2-Collections-STARTER-Nov24-master/src/main/java/org/example/Question6.java
@@ -1,15 +1,66 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Ewan Clarke
+ *  Class Group: SD2b
  */
 
-public class Question6      // Flight take-off (Queue)
-{
+public class Question6 { // Flight take-off (Queue)
+    // Creaing the queue for both
+    static Queue<String> takeOffQueue = new LinkedList<>();
+    static Queue<String> landQueue = new LinkedList<>();
 
-    public static void main(String[] args)
-    {
+    // Main function that goes to menu
+    public static void main(String[] args) {
+        flightMenu();
+    }
 
+    // Function for menu
+    public static void flightMenu() {
+        Scanner kb = new Scanner(System.in);
+        System.out.print("> ");
+        String input = kb.nextLine();;
+
+        if (input.contains("takeoff")) {
+            takeoff(input.substring(8));
+        } else if (input.contains("land")) {
+            land(input.substring(5));
+        } else if (input.equals("next")) {
+            next();
+        } else if (input.equals("quit")) {
+            System.exit(0);
+        }
+
+        System.out.print("\n");
+        flightMenu();
+    }
+
+    // Function to add to the takeoff queue
+    public static void takeoff(String flightCode) {
+        takeOffQueue.add(flightCode);
+        System.out.println("[+] Added flight " + flightCode + " to takeoff queue");
+    }
+
+    // Function to add to the land queue
+    public static void land(String flightCode) {
+        landQueue.add(flightCode);
+        System.out.println("[+] Added flight " + flightCode + " to land queue");
+    }
+
+    // Function to do the next resonable action
+    public static void next() {
+        if (!landQueue.isEmpty()) {
+            String landed = landQueue.poll();
+            System.out.println("[N] Flight " + landed + " has landed");
+        } else if (!takeOffQueue.isEmpty()) {
+            String tookOff = takeOffQueue.poll();
+            System.out.println("[N] Flight " + tookOff + " has taken off");
+        } else {
+            System.out.println("[N] Queue's are empty");
+        }
     }
 }
